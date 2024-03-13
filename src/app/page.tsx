4636c6d9bf438322100bd45cb8531a7ded056ae8
João Bebber <joao.gbebber@gmail.com>
2024-03-13 00:48:41 -0300

    :construction: Secure routes (not working)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 
+import { GetServerSideProps } from 'next'
 import Link from 'next/link'
+import { parseCookies } from 'nookies'
 import { useContext, useEffect } from 'react'
 
 import { Button } from '@/components/Button'
@@ -8,13 +10,14 @@ import { Container } from '@/components/Container'
 import { Title } from '@/components/Title'
 import { AuthContext } from '@/contexts/AuthContext'
 import { api } from '@/services/api'
+import { getAPIClient } from '@/services/axios'
 
 export default function Home() {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { user } = useContext(AuthContext)
 
   useEffect(() => {
-    api.get('/users')
+    // api.get('/users')
   }, [])
 
   return (
@@ -26,4 +29,25 @@ export default function Home() {
       </Link>
     </Container>
   )
+}
+
+// NAO SUPORTADO!
+export const getServerSideProps: GetServerSideProps = async (ctx) => {
+  const apiClient = getAPIClient(ctx)
+  const { '@medcalc.token': token } = parseCookies(ctx)
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      }
+    }
+  }
+
+  await apiClient.get('/users')
+
+  return {
+    props: {}
+  }
 }
--- src/services/api.ts
+++ src/services/api.ts
@@ -1,18 +1,4 @@
-import axios from 'axios'
-import { parseCookies } from 'nookies'
+import { getAPIClient } from './axios'
 
-const { '@medcalc.token': token } = parseCookies()
-
-export const api = axios.create({
-  baseURL: 'http://localhost:3333'
-})
-
-// api.interceptors.request.use(config => {
-//   console.log(config)
-  
-//   return config
-// })
-
-if (token) {
-  api.defaults.headers['Authorization'] = `Bearer ${token}`
-}
+// Chamada que faremos pro backend a partir do browser (sem contexto)
+export const api = getAPIClient()
